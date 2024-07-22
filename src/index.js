5e314e03606a76748681a339ba2efe13e71477d9
Sherlyne <sherlynea1994@hotmail.com>
2024-07-22 18:15:44 +0300

    Fetch lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+inputForm.addEventListener("submit", (event)=>{
+  event.preventDefault(); // stops the page from refreshing. 
+
+const inputForm = event.target.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies"/${inputForm.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+    const summary = document.querySelector("section#movieDetails p");
+    
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+
+    inputForm.value = ""; 
+
+});
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
